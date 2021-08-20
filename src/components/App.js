fb441f9c0b735b6ceb22b9254d414a65049b30cb
Catherine Loesch <catherine.loesch@protonmail.com>
2021-08-20 17:51:41 +0100

    assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import QuestionList from "./QuestionList";
 function App() {
   const [page, setPage] = useState("List");
 
+
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
@@ -15,3 +16,6 @@ function App() {
 }
 
 export default App;
+
+
+
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
-  return (
-    <section>
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(res => setQuestions(res))
+  },[])
+
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE" })
+      .then(res => res.json())
+      .then(() => {setQuestions(questions.filter((q) => (q.id !== id)))})
+  }
+
+  function handleAnswerChange(correctIndex, id) {
+    
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex })
+    })
+    .then(res => res.json())
+    .then((res) => { setQuestions(questions.map((q) => {
+        if (q.id === res.id) {
+          return res
+        } else {
+          return q
+        }
+      }))
+    })
+
+  }
+
+  
+  return (<section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((q) => (<QuestionItem question={q} key={q.id} handleDelete={handleDelete} handleAnswerChange={handleAnswerChange}/>))}
+      </ul>
+
     </section>
   );
 }
